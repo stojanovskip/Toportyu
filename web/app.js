b98089402998531caf7f9caca13c5d9276833965
AndrasTimar <andras.timar@odigeo.com>
2016-04-08 14:35:16 +0200

    changed this to , using then() instead of success() on promise

--- web/app.js
+++ web/app.js
@@ -1,16 +1,16 @@
 (function()
 {
 	var app = angular.module("myApp",[]);
-	app.controller("OrderController", ['$http',function($http){
-		var store = this;
-		this.orders=[];
-		this.buttonText= "Load";
-		this.orderToSend="";
-		this.load=function(){
-			$http.get('http://localhost:8000/orders').success(function(data){
-				store.orders = data.orderList;
-				console.log(data);
-				store.buttonText = "Refresh";
+	app.controller("OrderController", ['$http','$scope',function($http,$scope){
+		$scope.orders=[];
+		$scope.buttonText= "Load";
+		$scope.orderToSend="";		
+		$scope.load=function(){
+
+			 var promise = $http.get('http://localhost:8000/orders').then(function(response){
+				$scope.orders = response.data.orderList;
+				console.log(response.data);
+				$scope.buttonText = "Refresh";
 			})};
 		
 		}]);
