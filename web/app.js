ce7d62aa23e89d66f96326e44769afec57bf8189
AndrasTimar <andras.timar@odigeo.com>
2016-04-08 15:04:57 +0200

    introduced state instead of buttontext

--- web/app.js
+++ web/app.js
@@ -3,14 +3,14 @@
 	var app = angular.module("myApp",[]);
 	app.controller("OrderController", ['$http','$scope',function($http,$scope){
 		$scope.orders=[];
-		$scope.buttonText= "Load";
+		$scope.state= "notLoaded";
 		$scope.orderToSend="";		
 		$scope.load=function(){
 
 			 var promise = $http.get('http://localhost:8000/orders').then(function(response){
 				$scope.orders = response.data.orderList;
 				console.log(response.data);
-				$scope.buttonText = "Refresh";
+				$scope.state = "loaded";
 			})};
 		
 		}]);
