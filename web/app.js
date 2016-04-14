d4efeacfd5798fe2f5532be72d53931b600b048d
AndrasTimar <andras.timar@odigeo.com>
2016-04-14 15:05:00 +0200

    added polling

--- web/app.js
+++ web/app.js
@@ -1,25 +1,27 @@
 (function()
 {
 	var app = angular.module("myApp",[]);
-	app.controller("OrderController", ['$http','$scope',function($http,$scope){
+	app.controller("OrderController", ['$http','$scope','$timeout',function($http,$scope,$timeout){
 		$scope.orders=[];
 		$scope.newOrder ={};
+		$scope.counter = 0;
 		$scope.state= "notLoaded";
 		$scope.load=function(){
-			$http.get('/orders').then(function(response){
+			$timeout(function(){
+				$http.get('/orders').then(function(response){
 				$scope.orders = response.data.orderList;
 				console.log(response.data);
 				$scope.state = "loaded";
-			})};
+				$scope.counter++;
+				$scope.load();
+			})},1000)};
 
 		$scope.send = function(){
 			$http.post('/orders',$scope.newOrder).then(function(response){
 					$scope.orders.push($scope.newOrder);
 					$scope.newOrder = {};
 			}, function(response){
-			})
-			
-		};
+			})};
 
 		}]);
 })();
