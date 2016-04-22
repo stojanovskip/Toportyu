ddfb0ed55577b0246789610734abf1c3aeb5ade3
AndrasTimar <andras.timar@odigeo.com>
2016-04-22 13:36:46 +0200

    added port information to config file in webserver

--- webserver/main.js
+++ webserver/main.js
@@ -1,9 +1,13 @@
  var express = require('express');
   var proxy = require('express-http-proxy');
   var app = express();
-  
+  var PropertiesReader = require('properties-reader');
+  var properties = PropertiesReader('./resources/port.config');
+  var serverurl = properties.get('serverurl');
+  var webport = properties.get('webport');
+
   app.use(express.static('../web'));
   
-  app.use('/', proxy('localhost:8000'));
+  app.use('/', proxy(serverurl));
  
- app.listen(3000);
+ app.listen(webport);
