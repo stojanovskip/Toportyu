c4c8436a632e9609c43782ce24c00c512d94305f
AndrasTimar <andras.timar@odigeo.com>
2016-04-22 13:30:58 +0200

    Added server port to config file

--- server/src/Main/java/com/company/HttpInput.java
+++ server/src/Main/java/com/company/HttpInput.java
@@ -3,18 +3,25 @@ package com.company;
 import com.google.inject.Inject;
 import com.sun.net.httpserver.HttpServer;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Properties;
+
+import static java.lang.Integer.parseInt;
 
 /**
  * Created by bandi on 26/03/16.
  */
 class HttpInput {
     private final HttpServer httpServer;
-    private final int PORT = 8000;
+    private int PORT;
 
     @Inject
     HttpInput(Interactor interactor, IOrderTransformer orderTransformer) throws IOException {
+        Properties properties = new Properties();
+        properties.load(new FileInputStream("src\\Main\\resources\\port.config"));
+        PORT = parseInt(properties.getProperty("port"));
         httpServer = HttpServer.create(new InetSocketAddress(PORT), 0);
         httpServer.createContext("/orders", new ListHttpHandler(interactor, orderTransformer));
         httpServer.setExecutor(null);
