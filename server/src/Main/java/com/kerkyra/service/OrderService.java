08d3241826185612b34d8561a998a3817865e4c1
AndrasTimar <andras.timar@odigeo.com>
2016-04-26 18:29:10 +0200

    Added OrderServiceTest

--- server/src/Main/java/com/kerkyra/service/OrderService.java
+++ server/src/Main/java/com/kerkyra/service/OrderService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderService implements IOrderService {
+
     @Autowired
     OrderRepository orderRepository;
 
---
+++ server/src/test/java/com/kerkyra/service/OrderServiceTest.java
@@ -0,0 +1,52 @@
+package com.kerkyra.service;
+
+import com.kerkyra.model.Order;
+import com.kerkyra.repository.OrderRepository;
+import com.kerkyra.service.OrderService;
+import org.aspectj.weaver.ast.Or;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.kerkyra.Application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by Andras.Timar on 4/26/2016.
+*/
+ @ContextConfiguration(classes = Application.class)
+ @RunWith(SpringJUnit4ClassRunner.class)
+ public class OrderServiceTest {
+
+    @Mock
+    OrderRepository orderRepository;
+
+    @InjectMocks
+    OrderService orderService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        List<Order> orders = new ArrayList<>();
+        orders.add(new Order());
+        orders.add(new Order());
+        when(orderRepository.findAll()).thenReturn(orders);
+    }
+
+    @Test
+    public void getOrders() throws Exception {
+        Assert.assertEquals(2, ((List<Order>)orderService.getOrders()).size());
+    }
+
+}
