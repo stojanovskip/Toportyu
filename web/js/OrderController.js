4a979758ed8e3915c8cbda5fcf8937b32f3ea068
Meruyert <meruyert.bekzhigit@odigeo.com>
2016-06-16 17:03:39 +0200

    modified OrderController and improved tests

--- web/js/OrderController.js
+++ web/js/OrderController.js
@@ -1,7 +1,8 @@
-function OrderController ($scope, OrderService, CurrentState) {
+function OrderController($scope, OrderService, CurrentState, AuthenticationService, EventHandler) {
     $scope.orders = [];
     $scope.newOrder = {};
     $scope.showOrders = false;
+    $scope.showInputOrder = false;
 
     $scope.getOrders = function () {
         if (CurrentState.getCurrentTrip() !== null) {
@@ -12,21 +13,45 @@ function OrderController ($scope, OrderService, CurrentState) {
     };
 
     $scope.saveOrder = function () {
-        if ($scope.newOrder.cost !== undefined && $scope.newOrder.content !== undefined) {
-            $scope.newOrder.trip = CurrentState.getCurrentTrip();
-            return OrderService.saveOrder($scope.newOrder).then(function (respondedOrder) {
-                $scope.orders.push(respondedOrder);
-                $scope.newOrder = {};
-            });
-        }
+       return AuthenticationService.ensureUser().then(function (res) {
+            if ($scope.newOrder.cost !== undefined && $scope.newOrder.content !== undefined) {
+                $scope.newOrder.trip = CurrentState.getCurrentTrip();
+                return OrderService.saveOrder($scope.newOrder).then(function (respondedOrder) {
+                    $scope.orders.push(respondedOrder);
+                    $scope.newOrder = {};
+                });
+            }
+        });
     };
 
+    $scope.hideOrder = function () {
+        $scope.showInputOrder = false;
+    };
+    $scope.showOrder = function () {
+        $scope.showInputOrder = true;
+    };
+    EventHandler.on('after_logout', function () {
+        $scope.hideOrder();
+    });
+    EventHandler.on('after_login', function () {
+        if (CurrentState.getCurrentTrip() !== null)
+        $scope.showOrder();
+    });
+    
     $scope.$watch(function () {
         return CurrentState.getCurrentTrip();
     }, function (selectedTrip) {
         if (selectedTrip !== null) {
             $scope.getOrders();
             $scope.showOrders = true;
+            AuthenticationService.getCurrentUser().then(function (newUser) {
+                if (newUser.username !== null) {
+                    $scope.showInputOrder = true;
+                }
+                else {
+                    $scope.showInputOrder = false;
+                }
+            });
         }
     });
 }
