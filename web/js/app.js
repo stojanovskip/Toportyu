4198f0ec429a0b09b01094b857d2fdcc05acad38
stojanovskip <petar05skopje@hotmail.com>
2016-06-16 17:10:36 +0200

    AuthenticationService added to app.js

--- web/js/app.js
+++ web/js/app.js
@@ -1,10 +1,11 @@
 require('style!raw!sass!../scss/style.scss');
 
 var angular = require('angular');
-var app = angular.module('KerkyraApp',[]);
+var app = angular.module('KerkyraApp', []);
 
 require('./OrderController.js').install(app);
 require('./OrderService.js').install(app);
 require('./TripController.js').install(app);
 require('./TripService.js').install(app);
 require('./CurrentState.js').install(app);
+require('./AuthenticationService.js').install(app);
